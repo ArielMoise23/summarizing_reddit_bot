29ee1a24d43870040455a31e4fa78ffe568b426b
ariel.m23 <ariel.m23@gmail.com>
2024-03-19 09:30:33 -0500

    summarize text

--- reddit_bot.py
+++ reddit_bot.py
@@ -4,6 +4,9 @@ import re
 
 import requests
 from bs4 import BeautifulSoup
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
 
 
 def main():
@@ -24,15 +27,16 @@ def process_comment(comment):
     verified_link = identify_link(comment.body)
 
     if verified_link:
-        summerized_text = summerize_link(verified_link[0]) 
         # handle multiples links
+        summerized_text = summerize_link(verified_link[0])
+        commented = bot_commenting(summerized_text, verified_link[0])
+        return "summarized commented" if commented else "failed to comment"
 
-    return
+    return "no link"
 
 def identify_link(comment):
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex, comment)
-    [print(comment) if url else '']
     return [x[0] for x in url] if len(url) else False
 
 def scrap_info(link):
@@ -44,15 +48,33 @@ def scrap_info(link):
 
 def summerize_link(link):
     #iterate over links
-    #summerized text of the link.
     website_text = scrap_info(link)
-    
-    return 
 
-def bot_commenting(summerized_link_text):
+    # Parse the input text
+    parser = PlaintextParser.from_string(website_text, Tokenizer("english"))
+
+    # Create an LSA summarizer
+    summarizer = LsaSummarizer()
+
+    # Generate the summary
+    summary = summarizer(parser.document, sentences_count=3)  # You can adjust the number of sentences in the summary
+
+    summary_list = []
+    for sentence in summary:
+        summary_list.append(' '.join([word for word in sentence.words]))
+
+    return ' \n'.join(summary_list)
+
+def bot_commenting(summerized_link_text, link):
     ## posting a new comment with the summerized text
     ## comment.reply()
-    return
+
+    message_text = f""" hello, im a summarizer bot. i recognized a link in your comment - {link} and summarized it for you comfort (:
+        summary - 
+        {summerized_link_text}
+    """
+
+    return False
 
 if __name__ == "__main__":
     main()
