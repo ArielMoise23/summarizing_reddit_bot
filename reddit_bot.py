bceb728746751809f91d5c045b1439c2139744ac
ariel.m23 <ariel.m23@gmail.com>
2024-03-19 08:39:07 -0500

    scrap website

--- reddit_bot.py
+++ reddit_bot.py
@@ -2,6 +2,9 @@ import praw
 import os
 import re
 
+import requests
+from bs4 import BeautifulSoup
+
 
 def main():
     # All reddit api info
@@ -21,18 +24,29 @@ def process_comment(comment):
     verified_link = identify_link(comment.body)
 
     if verified_link:
-        summerized_text = summerize_link(verified_link)
+        summerized_text = summerize_link(verified_link[0]) 
+        # handle multiples links
 
     return
 
 def identify_link(comment):
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex, comment)
+    [print(comment) if url else '']
     return [x[0] for x in url] if len(url) else False
+
+def scrap_info(link):
+    # scraps text of website
+    ## need to identify the type of website article or offical
+    r = requests.get(link)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup.text.replace("\n", "")
 
 def summerize_link(link):
     #iterate over links
     #summerized text of the link.
+    website_text = scrap_info(link)
+    
     return 
 
 def bot_commenting(summerized_link_text):
